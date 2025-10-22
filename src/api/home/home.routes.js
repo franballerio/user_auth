c1f015dc5471c0cc3c87c31ea6044b73906cd560
Francisco Oscar Ballerio <fballerio@dc.uba.ar>
2025-10-22 08:35:26 -0300

    Register flow fixed by the moment (#5)

--- src/api/home/home.routes.js
+++ src/api/home/home.routes.js
@@ -4,20 +4,25 @@ export const homeRouter = Router()
 
 homeRouter.get('/', (req, res) => {
   const { userData } = req.session
-  if (!userData) return res.render('index')
 
-  try {
-    res.render('index', userData)
-  } catch (e) {
-    e
+  if (!userData) {
+    console.log('[INFO] no-auth user entering login page')
+    return res.render('index')
   }
+
+  console.log('[INFO] auth user entering login page')
+  return res.render('index', userData)
 })
 
 homeRouter.get('/protected', (req, res) => {
   const { userData } = req.session
-  if (!userData)
-    return res.render('protected')
 
+  if (!userData) {
+    console.log('[INFO] no-auth user entering protected page')
+    return res.render('protected')
+  }
+
+  console.log('[INFO] auth user entering protected page')
   return res.render('protected', userData)
 })
 
@@ -27,7 +32,12 @@ homeRouter.get('/reset', (req, res) => {
 
 homeRouter.get('/newPassword', (req, res) => {
   const { userData } = req.session
-  if (!userData) return res.status(403).send('Access denied')
 
-  return res.render('change-password', userData)
+  if (!userData) {
+    console.log('[INFO] no-auth user trying reset-password page. Acces denied')
+    return res.status(403).send('Access denied')
+  }
+
+  console.log('[INFO] auth user entering new-password page')
+  return res.render('newPassword', userData)
 })
